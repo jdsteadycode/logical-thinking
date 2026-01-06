af013fc1a5f99c6926e11346598a9ca6869caa43
jdsteadycode <desaijeet11@gmail.com>
2026-01-06 21:04:04 +0530

    updated logic for find sub folders of an sub-folder if available.

--- how-many-directories-and-contents/script.php
+++ how-many-directories-and-contents/script.php
@@ -29,17 +29,18 @@ function getSubDirectories(int $parentId, array $data) {
           // check log
           // var_dump($dir['id'], getSubDirectories($dir['id'], $data));
           
+          // check log
         
           // save each sub dir..
-          $subDirs[$index] = [
+          $subDirs[] = [
               "name" => $dir['name'],
               "id" => $dir['id'],
-              "sub_dirs" => [],
+              "sub_dirs" => getSubDirectories($dir['id'], $data),
           ];
           
           
-          // get sub dirs of sub dir itself available..
-          $subDirs[$index]['sub_dirs'] = getSubDirectories($dir['id'], $data);
+          // // get sub dirs of sub dir itself available..
+          // $subDirs[]['sub_dirs'] = getSubDirectories($dir['id'], $data);
         
         }
         
@@ -85,3 +86,90 @@ function getDirStructure(array $data) {
 
 // TEST
 // echo json_encode(folderStructure($directories), JSON_PRETTY_PRINT);
+
+// get the folder data..
+$data = getDirStructure($directories);
+
+// () -> render sub directories..
+function renderSubDirectories(array $dirs) {
+
+    // initial clutter 
+    $clutter = "";
+
+    // if dirs are empty
+    if(empty($dirs)) {
+        return "";
+    }
+
+    // iterate over sub directories..
+    foreach($dirs as $dir) {
+
+        // get sub directories of child also
+        $subDirs = renderSubDirectories($dir['sub_dirs']);
+        
+        // check log..
+        // var_dump($subDirs);
+
+        // get name of child dir..
+        $dirName = $dir['name'];
+
+        // save each dir data in clutter..
+        $clutter .= <<<HTML
+            <ul>
+                <li>$dirName</li>
+                <ul>$subDirs</ul>
+            </ul>
+        HTML;
+    }
+
+    // get html with data.
+    return $clutter;
+}
+
+// () -> render directories..
+function renderDirectories(array $data) {
+
+    // initial html
+    $clutter = "";
+
+    // iterate over the data..
+    foreach($data as $dir) {
+
+        // get name of root dir..
+        $rootDirName = $dir['name'];
+        $rootDirId = $dir['id'];
+
+        // get sub directories associated with..
+        $subDirs = renderSubDirectories($dir['sub_dirs']);
+
+        // add basic dir details to clutter
+        $clutter .= <<<HTML
+            <ul>
+                <li>$rootDirName</li>
+                $subDirs
+            <ul>
+        HTML;  
+    }
+
+    // get the data with clutter..
+    return $clutter;
+}
+
+
+// check..
+$dirs = renderDirectories($data);
+?>
+
+<html>
+    <body>
+        <h1>HTML CONTENT</h1>
+
+        <div>
+            <ul>
+                <?= $dirs ?>
+            </ul>
+        </div>
+    </body/>
+</html>
+
+
